270c05ca5d84a36c937e6835ea5b06675bd57b44
이승호 <82797695+koelkorea@users.noreply.github.com>
2023-04-25 18:40:09 +0900

    25일 작성중

--- day11-3(인접리스트로 graph구현).c
+++ day11-3(인접리스트로 graph구현).c
@@ -4,82 +4,95 @@
 //       -> (방법) 각 정점에서 자신에게 연결된 정점들에 대해서만 linkedlist 방식을 통해 연결주소를 붙혀둠
 
 // (모범예시) 인접리스트을 통한 graph 구현 - 연결리스트를 이용한 간선 저장 (0 1 2 삼각형에 3이 2에 연결된 구조)
-
-//정점들이 다닐 수 있는 길 -> 간선저장
-//연결리스트를 이용한 저장
 #include <stdio.h>
 #include <stdlib.h>
 
-// 노드 구조체
+// 정점 구조체(= 정점도 노드므로 node구조체로 go)
 typedef struct node {
 
-    //정점
-    int vertex;    
+    // 정점 index
+    int vertex;
 
-    //다음 노드의 주소
-    struct node* next;    
+    // 연결된 다른 노드가 위치한 메모리 주소 (= 정점들이 다닐 수 있는 간선들 = 동적할당을 통한 노드의 배열화로 간선저장효과 낼수 있음)
+    struct node* next_node;
+
 }node;
 
 // 그래프 구조체
 typedef struct graph {
 
-    //동적 할당된 노드배열의 주소 저장(출발지 노드)
-    node* head;   
+    // 동적 할당된 노드배열의 시작주소 저장(출발지 노드)
+    node* node_array;
 
-    //정점의 개수
-    int vertexCount;    
+    // 해당 그래프의 정점들 총 갯수
+    int vertexCount;
+
 }graph;
 
 // 그래프 구조체 초기화(그래프 구조체 메모리 위치주소, 원하는 정점의 갯수를 파라미터)
-void graphInitialize(graph* p, int count) {
+void graphInitialize(graph* graph, int count) {
 
     // 정점의 갯수를 그래프 정보에 입력 
-    p->vertexCount = count;
+    graph->vertexCount = count;
 
-    // 시작노드 정보 담을 구조체 동적 생성 = 노드들의 갯수에 해당하는 만큼, 구조체의 메모리 동적할당 후 그 주소를 그래프 구조체의 head에 대입
-    p->head = (node*)calloc(count, sizeof(node));
+    // 동적 정점 배열을 구현하고자, 정점 갯수들에 해당하는 만큼 메모리 크기 동적할당 후
+    //  -> 그 시작 주소를 그래프 구조체의 시작점(node_array)에 대입
+    graph->node_array = (node*)calloc(count, sizeof(node));
 
-    // 정점의 번호 동적 노드배열에 정보 입력
+    // 그래프 구조체의 멤버인 동적 정점배열의 각 요소에 각 정점의 index만 입력
+    //  -> 각 정점의 연결된 정점정보(= 간선정보)는 추후 메서드(addDirectedEdge, addUnDirectedEdge)를 통해 입력
     for (int i = 0; i < count; i++) {
 
-        p->head[i].vertex = i;
-        p->head[i].next = NULL;
+        graph->node_array[i].vertex = i;
+        graph->node_array[i].next_node = NULL;
     }
 }
 
 //방향 그래프(단방향) 간선 추가
-void addDirectedEdge(graph* p, int src, int dst) {
+void addDirectedEdge(graph* graph, int node_index, int node_destination) {
 
+    // 도착노드에 해당하는 노드의 순번이 없을때, 정점을 추가하기 위한 node구조체 동적할당 후, 그 node 시작 주소
     node* newNode = (node*)malloc(sizeof(node));
 
-    newNode->vertex = dst;
-    newNode->next = NULL;
+    newNode->vertex = node_destination;
+    newNode->next_node = NULL;
 
-    //노드가 없을 때 
-    if (p->head[src].next == NULL) { 
+    // 출발노드에 연결된 간선이 없을 때 
+    if (graph->node_array[node_index].next_node == NULL) {
 
-        p->head[src].next = newNode;
-        return;
+        // 도착점으로 설정된 정점 자신의 주소를 시작정점의 다음 정점의 주소에 대입
+        graph->node_array[node_index].next_node = newNode;
+
     }
+    // 출발노드에 연결된 간선이 있을 때?
+    //  -> 출발노드와 기존 연결노드의 사이에 자기가 끼어들어 출발노드와 자신의 연결정보를 변경(최종적으로는 연결된 놈들끼리는 순환구조.. )
+    else {
 
-    //맨 앞 추가
-    newNode->next = p->head[src].next;
+        // 출발정점에 있던 다음정점의 주소를 새로 생성된 도착정점의 다음 정점으로 대입
+        newNode->next_node = graph->node_array[node_index].next_node;
 
-    p->head[src].next = newNode;
+        // 그 후 도착점으로 설정된 정점 자신의 주소를 시작정점의 다음 정점의 주소에 대입
+        graph->node_array[node_index].next_node = newNode;
+
+    }
 }
 
 //무방향 그래프 (방향 그래프를 시작, 끝 반전시켜 추가)
-void addUnDirectedEdge(graph* p, int src, int dst) {
-    addDirectedEdge(p, src, dst);
-    addDirectedEdge(p, dst, src);
+void addUnDirectedEdge(graph* graph, int node_index, int node_destination) {
+    addDirectedEdge(graph, node_index, node_destination);
+    addDirectedEdge(graph, node_destination, node_index);
 }
 
-void display(graph* p) {
-    node* curNode;
-    for (int i = 0; i < p->vertexCount; i++) {
+void display(graph* graph) {
 
-        curNode = p->head[i].next;
-        printf("출발지 노드 : %d => ", p->head[i].vertex);
+    // 현재 위치 정점
+    node* curNode;
+
+    for (int i = 0; i < graph->vertexCount; i++) {
+
+        curNode = graph->node_array[i].next_node;
+
+        printf("출발지 노드 : %d => ", graph->node_array[i].vertex);
 
         //다음 노드로
         while (curNode != NULL) {
@@ -87,7 +100,7 @@ void display(graph* p) {
             printf("%d ", curNode->vertex);
 
             //다음 노드 주소값 저장  
-            curNode = curNode->next; 
+            curNode = curNode->next_node;
         }
         printf("\n");
     }
@@ -99,25 +112,32 @@ void memoryFree(graph* p) {
 
     for (int i = 0; i < p->vertexCount; i++) {
 
-        while (p->head[i].next != NULL) {
-            delNode = p->head[i].next; //delNode변경
-            p->head[i].next = delNode->next; //head변경
+        while (p->node_array[i].next_node != NULL) {
+
+            delNode = p->node_array[i].next_node; //delNode변경
+            p->node_array[i].next_node = delNode->next_node; //start_point변경
+
             printf("출발지 %d, 도착지 %d노드 제거\n", i, delNode->vertex);
+
             free(delNode); //노드 제거
         }
     }
-    free(p->head);
+    free(p->node_array);
     printf("헤드 노드 제거\n");
 }
 
 int main() {
+
     graph grp;
     graphInitialize(&grp, 4); //그래프, 정점의 개수
+
     addUnDirectedEdge(&grp, 0, 1);
     addUnDirectedEdge(&grp, 0, 2);
     addUnDirectedEdge(&grp, 1, 2);
     addUnDirectedEdge(&grp, 2, 3);
+
     display(&grp);
     memoryFree(&grp);
+
     return 0;
 }
