2a38d443e1278ba6f3991ab564322577479d4485
이승호 <82797695+koelkorea@users.noreply.github.com>
2023-04-26 17:47:46 +0900

    26일 grpah와 dfs 정리

--- day11-3(인접리스트로 graph구현).c
+++ day11-3(인접리스트로 graph구현).c
@@ -1,7 +1,8 @@
 // 그래프를 표현하는 방법
 //  2. 인접 리스트
 //     : 각 정점에 인접한 정점들을 연결리스트로 표현
-//       -> (방법) 각 정점에서 자신에게 연결된 정점들에 대해서만 linkedlist 방식을 통해 연결주소를 붙혀둠
+//       -> (방법) 각 정점에서 자신에게 연결된 정점들에 대해서만 linkedlist 방식을 통해 생성된 정점을 만들어 연결주소를 붙혀두고, 그 연결에 다른 정점과 연결된게 확인되면, 마찬가지로 또 정점을 생성해서 붙이는 방식
+//           -> 그렇게 모든 정점을 대상으로 자신에게서 연결된 생성노드 정보를 출력해보면 줄줄이 소시지처럼 나옴
 
 // (모범예시) 인접리스트을 통한 graph 구현 - 연결리스트를 이용한 간선 저장 (0 1 2 삼각형에 3이 2에 연결된 구조)
 #include <stdio.h>
@@ -40,7 +41,7 @@ void graphInitialize(graph* graph, int count) {
     graph->node_array = (node*)calloc(count, sizeof(node));
 
     // 그래프 구조체의 멤버인 동적 정점배열의 각 요소에 각 정점의 index만 입력
-    //  -> 각 정점의 연결된 정점정보(= 간선정보)는 추후 메서드(addDirectedEdge, addUnDirectedEdge)를 통해 입력
+    //  -> 각 정점의 연결된 정점정보(= 간선정보)는 추후 메서드(addDirectedEdge, addUnDirectedEdge)를 통해 인접정보 표기만을 위해 생성된 노드들과 연결됨
     for (int i = 0; i < count; i++) {
 
         graph->node_array[i].vertex = i;
@@ -48,33 +49,51 @@ void graphInitialize(graph* graph, int count) {
     }
 }
 
-//방향 그래프(단방향) 간선 추가
+//방향 그래프(단방향) 간선 추가 
+// -> (중요!) 정확히는 진짜 정점을 추가하는게 아니라, 어떤 정점의 인접정점 리스트에 누가누가 붙어있다는 내용을 추가하기 위한 것이니 여러개 만들어도 상관없음
+//     -> 그러니까 간선을 연결한다고, next_node에 진짜로 초기화 때 설정된 정점들과 정점들을 연결하는게 아니라, 
+//        그 정점의 연결리스트만을 위해 만들어진 또 다른 노드 구조체들과 연결되는 것을 알아두면 대가리가 안 복잡해짐 
+//        (= 그렇게 간선이 연결되면 또 다른 생성노드와 생성노드끼리 연결되서 어떤 정점과 연결된 정점들이 줄줄이 소시지 처럼 나옴)
 void addDirectedEdge(graph* graph, int node_index, int node_destination) {
 
-    // 도착노드에 해당하는 노드의 순번이 없을때, 정점을 추가하기 위한 node구조체 동적할당 후, 그 node 시작 주소
+    // 인접정점으로 추가할 정점 구조체 변수를 동적할당하고 주소위치를 받음
     node* newNode = (node*)malloc(sizeof(node));
 
+    // 인접정점으로 추가할 정점의 정보 입력 (연결정점의 역할로서 간선의 도착지 index 번호가 일단 들어감)
     newNode->vertex = node_destination;
     newNode->next_node = NULL;
 
-    // 출발노드에 연결된 간선이 없을 때 
-    if (graph->node_array[node_index].next_node == NULL) {
+    //----------------------------------------------------------------------------------------------
+    // 
+    //// 출발노드에 연결된 간선이 없음이 탐지?
+    //if (graph->node_array[node_index].next_node == NULL) {
 
-        // 도착점으로 설정된 정점 자신의 주소를 시작정점의 다음 정점의 주소에 대입
-        graph->node_array[node_index].next_node = newNode;
+    //    // 출발정점의 다음 정점의 주소에 도착점으로 설정된 정점 자신의 주소를 대입 
+    //    graph->node_array[node_index].next_node = newNode;
 
-    }
-    // 출발노드에 연결된 간선이 있을 때?
-    //  -> 출발노드와 기존 연결노드의 사이에 자기가 끼어들어 출발노드와 자신의 연결정보를 변경(최종적으로는 연결된 놈들끼리는 순환구조.. )
-    else {
+    //}
+    //// 출발정점으로 지정된 정점index에 기록된 인접정점 있음이 탐지? (끼워넣기 시작함)
+    ////  -> 출발정점으로 지정된 정점index에 기록된 인접정점 주소에 새로 만들어진 인접정점 자신의 주소를 연결 + 새로 만들어진 인접정점 자신의 인접정점에는 기존 출발정점으로 지정된 정점index에 기록된 인접정점을 대입
+    ////     -> 출발정점 => 기존 인접정점  to 출발정점 => 새 인접정점 => 기존 인접정점
+    //else {
 
-        // 출발정점에 있던 다음정점의 주소를 새로 생성된 도착정점의 다음 정점으로 대입
-        newNode->next_node = graph->node_array[node_index].next_node;
+    //    // 새로 생성된 도착정점의 다음 정점에 원래 출발정점에 있던 다음정점의 주소 대입
+    //    newNode->next_node = graph->node_array[node_index].next_node;
 
-        // 그 후 도착점으로 설정된 정점 자신의 주소를 시작정점의 다음 정점의 주소에 대입
-        graph->node_array[node_index].next_node = newNode;
+    //    // 그 후 출발정점의 다음 정점의 주소에 도착점으로 설정된 정점 자신의 주소를 대입 
+    //    graph->node_array[node_index].next_node = newNode;
 
-    }
+    //}
+    // 
+    //----------------------------------------------------------------------------------------------
+    // -> (중요) 사실 조건문 없어도 되는게, 간선의 시작정점의 기존 다음 정점이 없어도 로직 진행에는 전혀 문제가 없음
+
+    // 새로 생성된 도착정점의 다음 정점에 원래 출발정점에 있던 다음정점의 주소 대입 (시작정점의 다음 정점이 NULL이면 그냥 NULL이 들어가면 되니 문제 없음)
+    newNode->next_node = graph->node_array[node_index].next_node;
+
+    // 그 후 출발정점의 다음 정점의 주소에 도착점으로 설정된 정점 자신의 주소를 대입 
+    graph->node_array[node_index].next_node = newNode;
+
 }
 
 //무방향 그래프 (방향 그래프를 시작, 끝 반전시켜 추가)
@@ -83,33 +102,39 @@ void addUnDirectedEdge(graph* graph, int node_index, int node_destination) {
     addDirectedEdge(graph, node_destination, node_index);
 }
 
+// 현재 그래프 각 정점들의 인접정점들을 모두 보여줘서, 그래프 연결현황을 전부 파악 가능하게 함
 void display(graph* graph) {
 
     // 현재 위치 정점
     node* curNode;
 
+    // 각 정점마다 인접노드 토해내게 반복문
     for (int i = 0; i < graph->vertexCount; i++) {
 
+        // 현재 위치 정점 = 현 정점의 다음 정점주소
         curNode = graph->node_array[i].next_node;
 
         printf("출발지 노드 : %d => ", graph->node_array[i].vertex);
 
-        //다음 노드로
+        //출발정점 간선 따라가며, 연결된 정점들 다 출력하기
         while (curNode != NULL) {
 
             printf("%d ", curNode->vertex);
 
-            //다음 노드 주소값 저장  
+            // 다다음 정점 주소값 저장  
             curNode = curNode->next_node;
         }
         printf("\n");
     }
 }
 
+// 해당 그래프에 할당된 모든 메모리 수거.. 
+// -> 잊지 말아야 할게, 연결정점으로 만든거 하나하나가 다 할당된거니 전부 수거해야함
 void memoryFree(graph* p) {
 
     node* delNode;
 
+    // 그래프에 놓인 정점배열의 index에 해당하는 개별 요소에 붙어있는 연결리스트들을 다음 노드가 없을떄까지 모조리 추적해서 지움
     for (int i = 0; i < p->vertexCount; i++) {
 
         while (p->node_array[i].next_node != NULL) {
@@ -122,7 +147,11 @@ void memoryFree(graph* p) {
             free(delNode); //노드 제거
         }
     }
+
+    // 마지막으로 그래프 구조체를 위해 할당된 메모리까지 전부 수거하면 끝
+
     free(p->node_array);
+    
     printf("헤드 노드 제거\n");
 }
 
--- day11-4(DFS를 통한 미로탐색 문제).c
+++ day11-4(DFS를 통한 미로탐색 문제).c
@@ -1,65 +1,68 @@
-// ���� �켱 Ž�� (DFS : Depth First Search)
-//  : �׷����� �ϳ��� ����(���� root)���κ��� �����Ͽ�, ���ʴ�� ��� �������� �ѹ��� �湮
-//     -> ���� stack �ڷᱸ���� Ȱ���ؼ� ����
+﻿// 깊이 우선 탐색 (DFS : Depth First Search)
+//  : 그래프를 하나의 정점(보통 root)으로부터 시작하여, 차례대로 모든 정점들을 한번씩 방문
+//     -> 보통 stack 자료구조를 활용해서 구현
 
-//  # DFS�� ����Ͽ� �̷� Ž�� �����ذ� ����
-//     : �̷θ� Ž���� ��ó�� �� �������� �� �� ���� ������ ��� �����ϴ� ���
-//        -> �̸� �� �̻� �� �� ���� �Ǹ�, �ٽ� ���� ����� ������� ������ �ٸ� �������� �ٽ� Ž���� �ݺ��ϴ� ������ ������ �ذ��ϴ� �Ͱ� ������
+//  # DFS를 사용하여 미로 탐색 문제해결 예시
+//     : 미로를 탐색할 때처럼 한 방향으로 갈 수 있을 때까지 계속 진행하는 경우
+//        -> 이를 더 이상 갈 수 없게 되면, 다시 가장 가까운 갈림길로 복귀후 다른 방향으로 다시 탐색을 반복하는 식으로 문제를 해결하는 것과 유사함
 
-//    - �ذ�� 1) 
-//       : 'stack �ڷᱸ��'�� boolean������ �迭�� ����Ͽ� �ذ�
-//          (1) �ϳ��� ������ stack�� push�ص�
-//          (2) stack�� �������� �� ������ pop �ϰ�, �� ������ ���� �湮���üũ �迭�� �ش� ����׸��� �湮���� ���� (pop = print �� ������)
-//          (3) pop �� �������� �湮���� ���� ������ �������� �ٽ� stack�� push
-//          (4) stack�� �� ������ 2�� 3���� �ݺ��ϰ�, �� �̻� �湮���� ���� ������ ���ö����� �̸� ��� �ݺ� (whlie�� ���� ����)
+//    - 해결법 1) 
+//       : 'stack 자료구조'와 boolean형식의 배열을 사용하여 해결
+//          (1) 하나의 정점을 stack에 push해둠
+//          (2) stack이 빌때까지 그 정점을 pop 하고, 그 정점에 대한 방문노드체크 배열의 해당 노드항목을 방문으로 변경 (pop = print 로 간주함)
+//          (3) pop 된 정점에서 방문하지 않은 인접한 정점들을 다시 stack에 push
+//          (4) stack이 빌 때까지 2번 3번을 반복하고, 더 이상 방문하지 않은 정점이 나올때까지 이를 계속 반복 (whlie문 쓰면 쉽다)
 
-//    - �ذ�� 2)
-//       : '����Լ�'�� boolean������ �迭�� ����Ͽ� �ذ� (= ����Լ��� ���°� ���� stack�� ���� ��)
-//          -> �Լ��� �޸��� stack�������� �Լ��������� �����ϸ�, �� �ȿ��� �ٸ� �Լ��� ȣ���ϸ� stack������ ���̰� �������ϸ� LIFO��� �۵�
-//             (1) �ϳ��� ������ ������� ����
-//             (2) �湮 �޼��� ���� ��, �� ������ ���� �湮���üũ �迭�� �ش� ����׸��� �湮���� ���� (pop = print �� ������)
-//             (3) ���� �������� �湮���� ���� ������ �������� �ϳ��� ��� �̵�
-//             (4) 2�� 3���� �ݺ��ϰ� ���������� NULL�̸� ���� ������� ���ư��� ���, �� �̻� �湮���� ���� ������ ���ö����� �̸� ��� �ݺ�
+//    - 해결법 2)
+//       : '재귀함수'와 boolean형식의 배열을 사용하여 해결 (= 재귀함수를 쓰는거 또한 stack을 쓰는 셈)
+//          -> 함수는 메모리의 stack영역에서 함수프레임을 형성하며, 그 안에서 다른 함수를 호출하면 stack구조로 쌓이고 빠지고하며 LIFO방식 작동
+//             (1) 하나의 정점을 출발지로 정함
+//             (2) 방문 메서드 수행 후, 그 정점에 대한 방문노드체크 배열의 해당 노드항목을 방문으로 변경 (pop = print 로 간주함)
+//             (3) 현재 정점에서 방문하지 않은 인접한 정점들중 하나를 잡고 이동
+//             (4) 2번 3번을 반복하고 인접정점이 NULL이면 이전 갈림길로 돌아가서 계속, 더 이상 방문하지 않은 정점이 나올때까지 이를 계속 반복
 
 
-// DFS�� �̷� ���� ���� (STACK, ��� �Ѵ� ����)
+// DFS와 미로 예시 구현 (STACK, 재귀 둘다 포함)
 #include <stdio.h>
 #include <stdlib.h>
 
-// ��� ����ü
+// 정점(=노드) 구조체
 typedef struct node {
 
-    // ������
+    // 도착지
     int dst;
 
-    // ���� ����� �ּ� ����
+    // 다음 노드의 주소 저장
     struct node* next;
+
 }node;
 
-// �׷��� ����ü ()
-typedef struct graph {//������ �����ϱ� ���� �׷���
+// 그래프 구조체
+typedef struct graph {
 
-    // ������ ����
+    // 정점의 개수
     int count;
 
-    // ������ ������ŭ �Ҵ� �� node(�����)
+    // 정점의 개수만큼 할당한 node배열의 출발주소 (출발지)
     node* arr;
+
 }graph;
 
-// stack ����ü
+// stack 구조체
 typedef struct stack {
 
-    //�����Ϸ��� ��ġ
-    int top;    
+    // 현재 스택에 들어온 데이터의 수
+    int top;
 
-    //�����Ҵ�� ������ �ּҸ� ����
-    int* arr;    
+    // 동적할당된 스택의 주소를 저장
+    int* arr;
 
-    //������ �ִ� �뷮
-    int capacity;  
+    // 스택의 최대 용량
+    int capacity;
+
 }stack;
 
-// GRAPH �Լ�
+// GRAPH 함수
 void stack_initialize(stack* stk, int count);
 void push(stack* stk, int val);
 void graph_initialize(graph* grp, int count);
@@ -68,14 +71,17 @@ void addUndirectedEdge(graph* grp, int src, int dst);
 void graph_display(const graph* grp);
 void memoryFree(graph* grp);
 
-// STACK �Լ�
+// STACK 함수
 int pop(stack* stk);
 int getCapacity(const stack* stk);
 int getSize(const stack* stk);
 
-// DFS �Լ�
+// DFS 함수
+//----------------[재귀함수식]----------------
 void DFSReculsive(graph* grp, int index, int* visited);
 void DFS(graph* grp, int source);
+
+//----------------[stack식]----------------
 void DFS_stack(graph* grp, int source);
 
 int main() {
@@ -96,169 +102,267 @@ int main() {
 
     graph_display(&grp);
 
-    // �ذ�� 2) ��ȭ������ '����Լ�' ����� DFS
-    DFS(&grp, 0); 
+    // 해결법 2) 점화식으로 '재귀함수' 사용한 DFS
+    DFS(&grp, 0);
     printf("\n");
 
-    // �ذ�� 1) 'stack �ڷᱸ��' ����� DFS
-    DFS_stack(&grp, 0); 
+    // 해결법 1) 'stack 자료구조' 사용한 DFS
+    DFS_stack(&grp, 0);
     memoryFree(&grp);
     return 0;
 }
 
-//---------------------------------------[stack �Լ�]---------------------------------------
+//---------------------------------------[stack 함수]---------------------------------------
 
-//stack ����ü �ʱ�ȭ : ���� ��� �ʱ�ȭ
+//stack 구조체 초기화 : 구성 멤버 초기화
 void stack_initialize(stack* stk, int count) {
-    stk->arr = (int*)malloc(sizeof(int) * count);// int arr[count]; ���� �迭 ���� ���� �Ҵ�
-    stk->top = 0; // ���� ���� 0���� �ʱ�ȭ
-    stk->capacity = count; // �� �뷮 count �� ����
+
+    // int arr[count] : stack의 멤버로서 실질적으로 stack에 들어온 데이터값을 저장할 배열 공간 동적 할당
+    stk->arr = (int*)malloc(sizeof(int) * count);
+
+    // stack의 보유 개수 0으로 초기화
+    stk->top = 0; 
+
+    // 총 용량 count 개 설정
+    stk->capacity = count; 
+
 }
 
-// stack �޴ܿ� �� �߰�
+// stack 뒷단에 값 추가
 void push(stack* stk, int val) {
-    if (stk->top >= stk->capacity) { // ���� �뷮 �ʰ��� ���
+
+    // 현재 stack 용량이 가득찬 상태거나 초과시 경고
+    if (stk->top >= stk->capacity) { 
+
         printf("\n\n\t\tstack overflow\n");
+
         return;
     }
-    stk->arr[stk->top++] = val; // ���� arr �迭�� ������(dst) ���� �� top++
+
+    // 스택 arr 배열에 목적지(dst) 삽입 후 top++
+    stk->arr[stk->top++] = val; 
+
 }
 
-
+// stack의 가장 뒷단의 값 뺴기
 int pop(stack* stk) {
-    if (stk->top <= 0) { // ���� ����
+    if (stk->top <= 0) { // 공백 에러
         printf("\n\n\t\tstack underflow\n");
         return -1;
     }
-    return stk->arr[--(stk->top)]; // dst�� arr�迭���� ���� (pop)
+    return stk->arr[--(stk->top)]; // dst를 arr배열에서 삭제 (pop)
 }
 
-
-int getCapacity(const stack* stk) { // ���� �� �뷮 Ȯ�� �Լ�
+// 스택 총 용량 확인 함수
+int getCapacity(const stack* stk) {
     return stk->capacity;
 }
 
-
-int getSize(const stack* stk) { // ���� �ڷ� ���� Ȯ��
+// 현재 stack 내 데이터 개수 확인
+int getSize(const stack* stk) {
     return stk->top;
 }
 
 
-//---------------------------------------[graph �Լ�]---------------------------------------
+//---------------------------------------[graph 함수]---------------------------------------
 
-// �׷��� ����ü ���� ��� �ʱ�ȭ
-void graph_initialize(graph* grp, int count) { 
-    grp->count = count; // ���� ���� �ʱ�ȭ
-    grp->arr = (node*)calloc(count, sizeof(node)); // node arr[count]; ���� ���� ���� �Ҵ�
+// 그래프 구조체 구성 멤버 초기화
+void graph_initialize(graph* grp, int count) {
+
+    // 정점 갯수 초기화
+    grp->count = count; 
+
+    // node arr[count]; 개별 정점의 데이터들, 개수만큼 동적배열로 할당 후, 그 시작주소 그래프 구조체 node포인터변수 arr에 대입
+    grp->arr = (node*)calloc(count, sizeof(node)); 
+
 }
 
-// ���Ⱓ�� �׷����� �߰�
+// 방향간선 그래프에 추가
+// -> (중요!) 정확히는 진짜 정점을 추가하는게 아니라, 어떤 정점의 인접정점 리스트에 누가누가 붙어있다는 내용을 추가하기 위한 것이니 여러개 만들어도 상관없음
+//     -> 그러니까 간선을 연결한다고, next_node에 진짜로 초기화 때 설정된 정점들과 정점들을 연결하는게 아니라, 
+//        그 정점의 연결리스트만을 위해 만들어진 또 다른 노드 구조체들과 연결되는 것을 알아두면 대가리가 안 복잡해짐 
+//        (= 그렇게 간선이 연결되면 또 다른 생성노드와 생성노드끼리 연결되서 어떤 정점과 연결된 정점들이 줄줄이 소시지 처럼 나옴)
 void addDirectedEdge(graph* grp, int src, int dst) {
-    node* newNode = (node*)malloc(sizeof(node));
-    newNode->dst = dst;
-    newNode->next = grp->arr[src].next;
-    grp->arr[src].next = newNode;
-} // ���� ����Ʈ ��� �߰�, �������� ������ ����
 
-// �׷��� ����� ���� ����
-void addUndirectedEdge(graph* grp, int src, int dst) { 
+    // 인접정점으로 추가할 정점 구조체 변수를 동적할당하고 주소위치를 받음
+    node* newNode = (node*)malloc(sizeof(node));
+
+    // 인접정점으로 추가할 정점의 정보 입력 (연결정점의 역할로서 간선의 도착지 index 번호가 일단 들어감)
+    newNode->dst = dst;
+
+    // 새로 생성된 도착정점의 다음 정점에 원래 출발정점에 있던 다음정점의 주소 대입 (시작정점의 다음 정점이 NULL이면 그냥 NULL이 들어가면 되니 문제 없음)
+    newNode->next = grp->arr[src].next;
+
+    // 그 후 출발정점의 다음 정점의 주소에 도착점으로 설정된 정점 자신의 주소를 대입 
+    grp->arr[src].next = newNode;
+
+}
+
+// 그래프 양방향 간선 설정 (방향 그래프를 시작, 끝 반전시켜 추가)
+void addUndirectedEdge(graph* grp, int src, int dst) {
     addDirectedEdge(grp, src, dst);
     addDirectedEdge(grp, dst, src);
 }
 
-// �׷��� ���� ���
-void graph_display(const graph* grp) { // ��� ���� -> ���� ���� ǥ ���
+// 현재 그래프 각 정점들의 인접정점들을 모두 보여줘서, 그래프 연결현황을 전부 파악 가능하게 함
+void graph_display(const graph* grp) {
+
+    // 연결노드 추적시 현재의 노드위치를 받기 위한 변수
     node* cur = NULL;
+
     if (grp == NULL) return;
-    for (int i = 0; i < grp->count; i++) { //������ ������ŭ �ݺ�
-        cur = grp->arr[i].next; // 1�� ������ ���� ���� �ּ� ����
-        printf("����� ��� : %d ==> ", i); // ����� ��� ���
-        while (cur) { // ������� NULL �� Ȯ��(���� ����)
-            printf("%d ", cur->dst); // ������ ������ ���
-            cur = cur->next; // ���� ����
+
+    // 각 정점마다 인접노드 토해내게 반복문
+    for (int i = 0; i < grp->count; i++) {
+
+        // 현재 위치 정점 = 현 정점의 다음 정점주소
+        cur = grp->arr[i].next; 
+
+        printf("출발지 노드 : %d ==> ", i); // 출발지 노드 출력
+
+        //출발정점 간선 따라가며, 연결된 정점들 다 출력하기
+        while (cur) {
+
+            printf("%d ", cur->dst); 
+
+            // 다다음 정점 주소값 저장 
+            cur = cur->next; // 다음 노드로
         }
+
         printf("\n");
     }
 }
 
+// 해당 그래프에 할당된 모든 메모리 수거.. 
+// -> 잊지 말아야 할게, 연결정점으로 만든거 하나하나가 다 할당된거니 전부 수거해야함
 void memoryFree(graph* grp) {
+
     node* delNode;
+
+    // 그래프에 놓인 정점배열의 index에 해당하는 개별 요소에 붙어있는 연결리스트들을 다음 노드가 없을떄까지 모조리 추적해서 지움
     for (int i = 0; i < grp->count; i++) {
-        while (grp->arr[i].next) { // ���� ����Ʈ ��� ����
-            delNode = grp->arr[i].next; // ���� ��� �ּ� ����
-            grp->arr[i].next = delNode->next; // ���� ��� �ּҷ� ����
+
+        // 해당 정점에 더 이상 연결된 노드가 없을떄까지 동적할당 생성한 노드 삭제
+        while (grp->arr[i].next) { 
+
+            // 삭제할 연결된 노드 주소 저장
+            delNode = grp->arr[i].next; 
+
+            // 정점의 다음노드 주소를 다다음 노드 주소로 변경
+            grp->arr[i].next = delNode->next; 
+
             free(delNode);
         }
     }
-    free(grp->arr); // ����� ��� ����
+    free(grp->arr); // 출발지 노드 삭제
 }
 
-//---------------------------------------[DFS �Լ�]---------------------------------------
+//---------------------------------------[DFS 함수]---------------------------------------
 
 //--------------------------------------------------------------------------------------------------------------------------
-// �ذ�� 2) '����Լ�'�� boolean������ �迭�� ����Ͽ� �ذ� (= ����Լ��� ���°� ���� stack�� ���� ��)
+// 해결법 2) '재귀함수'와 boolean형식의 배열을 사용하여 해결 (= 재귀함수를 쓰는거 또한 stack을 쓰는 셈)
 
-// DFS ��ȭ�� ����
-void DFS(graph* grp, int source) { // �Ű����� : �׷���, �����
+// DFS 점화식 (매개변수 : 초기화한 그래프 구조체 주소위치, 출발지)
+void DFS(graph* grp, int source) {
 
-    int* visited = (int*)calloc(grp->count, sizeof(int)); //int visited[count]; boolean visited array ���� �湮 �迭
-    printf("%d ", source); //����� ���� ��� "0"
-    DFSReculsive(grp, source, visited); 
+    //=======================재귀 이전 세팅=======================
+
+    // int visited[count] : boolean visited array 정점 방문 배열을 동적할당 후, 그 시작주소 반환
+    int* visited = (int*)calloc(grp->count, sizeof(int)); 
+    printf("%d ", source);
+
+    //============================================================
+
+    // 재귀메서드 시작!
+    DFSReculsive(grp, source, visited);
 }
 
-//DFS���ȣ�� (�׷���, �����, �湮��)
-void DFSReculsive(graph* grp, int index, int* visited) {// �Ű����� : �׷���, �����, �湮��
+// DFS재귀호출 (그래프, 출발지, 방문지)
+void DFSReculsive(graph* grp, int index, int* visited) {
 
-    node* curNode = grp->arr[index].next; // ������� ����� ������ ������ ��� �ּ�(���� ����)
-    visited[index] = 1; // �湮�� üũ
+    // 현재 정점에 연결된 인접 정점의 노드 주소 (= 간선 추적)
+    node* curNode = grp->arr[index].next; 
 
-    while (curNode) { // ���� ���� ����
+    // 현재 정점을 방문했음을 체크
+    visited[index] = 1; 
 
-        int destination = curNode->dst; // ���� ���� �������� ����
+    // 현재 노드(인접정점)의 인접 정점이 없을때까지 반복
+    while (curNode) { 
 
-        if (visited[destination] == 0) { // �湮�� ���� ������
-            printf("%d ", destination); // ������ ���
-            DFSReculsive(grp, destination, visited); // �������� �������� ���� �������� ��� �Լ�
+        // 현재 노드(인접정점)의 인접 정점을 목적지로 설정
+        int destination = curNode->dst; 
+
+        // 그 인접정점에 해당하는 정점을 방문한 적이 없음이 판명되면?
+        if (visited[destination] == 0) { 
+
+            // 도착지로 설정된 정점의 index를 출력
+            printf("%d ", destination);
+
+            // 그 정점을 출발지로 설정하고 다음 목적지를 향해 재귀 ㄱㄱ
+            DFSReculsive(grp, destination, visited); 
         }
 
-        curNode = curNode->next; // �湮�� ���� ������ ���� ���� �ּҷ�
+        // 해당 노드에 방문한 적이 있으면, 그 노드의 다음 인접정점의 주소를 대입
+        curNode = curNode->next; 
     }
 }
 //--------------------------------------------------------------------------------------------------------------------
 
-// �ذ�� 1) �׷����� stack�� Ȱ���� DFS ������� �����ذ� 
-void DFS_stack(graph* grp, int source) { // �Ű����� : �׷���, �����
+// 해결법 1) 그래프를 stack을 활용한 DFS 방식으로 문제해결 
+//  -> stack에 출발점 정점의 주소를 투입(점화 시작)
+//     -> while 1번 : stack에서 정점을 하나씩 pop해서, 연결된 줄줄이 정점들을 stack에 넣어두고 언급된 노드는 방문처리하기
+//     -> while 2번 : 1번을 반복해서, 새로 방문한 정점의 연결된 줄줄이 정점들중 방문 안한 정점들만 stack에 넣어두고, stack이 텅텅 빌때까지 하기
+void DFS_stack(graph* grp, int source) { // 매개변수 : 그래프, 출발지
 
-    int* visited = (int*)calloc(grp->count, sizeof(int)); //int visited[count]; boolean visited array ���� �湮 �迭
+    // int visited[count] : boolean visited array 정점 방문 배열을 동적할당 후, 그 시작주소 반환
+    int* visited = (int*)calloc(grp->count, sizeof(int));
 
+    // stack 구조체 선언
     stack stk;
-    stack_initialize(&stk, 9); // stack �ʱ�ȭ �� �뷮 9��
-    push(&stk, source); //����� ���� ���ÿ� ����
-    visited[source] = 1; // �湮�� üũ
 
-    // ������ ������ ��� Pop �� ������ �ݺ�
+    // 해당 stack 용량 9개로 초기화
+    stack_initialize(&stk, 9);
+
+    //출발지 정점의 index를 스택에 삽입 (= stack에 출발점 정점의 주소를 투입(점화 시작))
+    push(&stk, source); 
+
+    // 출발지에 해당하는 노드 방문체크
+    visited[source] = 1;
+
+    // whlle의 목적(있는 stack의 정점 index값 빼내기)
+    //  : stack에 들어온 정점을 모두 Pop 할 때까지 반복
     while (getSize(&stk) != 0) {
 
-        int traversal = pop(&stk); // �湮�� ���� Pop
-        printf("%d ", traversal); // �湮�� ���� ���
-        node* curNode = grp->arr[traversal].next; // �湮�� ������ ���� ���� �ּ� ����
+        // stack에 들어온 첫번째 요소(= 방문한 정점) Pop
+        int startNode = pop(&stk); 
+        printf("%d ", startNode);
 
-        // ���� ���� �ּҰ� ���� ��
+        // 현재 노드의 위치를 받기 위한 node구조체의 포인터변수 생성 후, 방문한 정점의 인접 정점 주소
+        node* curNode = grp->arr[startNode].next;
+
+        // while의 목적(stack 채울 용도로 pop한 정점index와 연결된 정점 index 짜내서 방문안되었으면 stack에 넣기)
+        //  : 현재 정점에 인접 정점 주소가 있으면 계속 반복 (= 추후 그 현재 정점과 연결된 노드들이 줄줄이 다 나올때까지 계속 반복)
         while (curNode) {
 
-            // ���� ���� ������
+            // 현재 보고 있는 인접 정점의 index
             int dst = curNode->dst;
 
-            // �湮�� ���� ������
+            // 그 인접 정점에 방문한 적이 없으면
             if (visited[dst] == 0) {
 
-                push(&stk, dst); // ���ÿ� �ֱ�
-                visited[dst] = 1; // �湮�� üũ
+                // 현재 해당 인접정점의 index를 스택에 넣음
+                push(&stk, dst); 
+
+                // 현재 해당 인접정점을 방문처리
+                visited[dst] = 1; 
             }
 
-            curNode = curNode->next; // �湮�� ���� ������ ���� ���� �ּҷ�
+            // 그 인접 정점에 해당하는 정점에 방문한 적이 있으면, 그 인접정점의 다음 인접정점의 주소를 대입 (= 이렇게 한 정점의 연결 정점 리스트가 더 이상 없어 NULL이 나오면 종료)
+            curNode = curNode->next; 
         }
     }
+
     printf("\n");
-    free(stk.arr); //���� �޸� ����
+
+    free(stk.arr); //스택 메모리 해제
 }
